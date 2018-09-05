2be3ca231e8fc980bdf038694cbbfa46db51936b
abc <abc@example.com>
2018-09-05 16:13:57 -0700

    removed dummy data

--- src/app.js
+++ src/app.js
@@ -13,18 +13,7 @@ import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore();
 
-//
-// store.dispatch(addExpense({ description: 'Gas bill', amount: 4500, createdAt: 12000 }));
-// store.dispatch(addExpense({ description: 'Water bill', amount: 2600, createdAt: 1000 }));
-// store.dispatch(addExpense({ description: 'Rent bill', amount: 111600, createdAt: 5000 }));
 
-
-
-const state = store.getState();
-const filteredExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-
-store.dispatch(sortByDate())
 
 
 const jsx = (
