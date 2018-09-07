8566cd46b105263d3259dd0c4c277738dfe1105a
abc <abc@example.com>
2018-09-06 18:14:57 -0700

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,28 @@
+import * as firebase from 'firebase';
+
+
+
+const config = {
+    apiKey: "AIzaSyCECbr3Rlv04o01lK0hZSNLOIT7is0QpwY",
+    authDomain: "expensify-188bd.firebaseapp.com",
+    databaseURL: "https://expensify-188bd.firebaseio.com",
+    projectId: "expensify-188bd",
+    storageBucket: "expensify-188bd.appspot.com",
+    messagingSenderId: "510841147666"
+};
+
+
+
+firebase.initializeApp(config);
+
+
+const database = firebase.database();
+
+database.ref().set({
+  work: 'billy',
+  age: 13,
+  location:{
+    lit:34,
+    bit:4555
+  }
+})
